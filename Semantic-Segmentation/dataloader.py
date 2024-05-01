ce5c9963d9c79de6b0cf7e598af478320547207f
hasanmog <hmoughnieh2000@gmail.com>
2024-05-01 20:24:46 +0300

    dataloader

--- Semantic-Segmentation/dataloader.py
+++ Semantic-Segmentation/dataloader.py
@@ -1,30 +1,32 @@
 import os
 import torch.nn as nn
+from PIL import Image
 from torch.utils.data import Dataset
 
-class DeepGlobe_Road(Dataset):
-    def __init__(self , parent_dir : str , set : str , transform):
+class WHU_bldg(Dataset):
+    def __init__(self , parent_dir : str , set : str , transform_rgb = None , transform_grey = None):
         
-        self.path = os.path.join(parent_dir , set)
-        self.transform = transform
+        self.images_dir = os.path.join(parent_dir , set , 'Images')
+        self.masks_dir = os.path.join(parent_dir , set , 'Masks')
+        self.transform_rgb = transform_rgb
+        self.transform_grey = transform_grey
+        
+        self.images = [os.path.join(self.images_dir, f) for f in sorted(os.listdir(self.images_dir))]
+        self.masks = [os.path.join(self.masks_dir , f) for f in sorted(os.listdir(self.masks_dir))]
+        
     def __len__(self):
+        return len(self.images)
         
-        files = os.listdir(self.path)
-        return int(len(files) /  2)
+    def __getitem__(self , idx ):
         
-    def __getitem__(self ):
+        image = Image.open(self.images[idx]).convert("RGB")
+        mask = Image.open(self.masks[idx]).convert("L")
         
-        data = os.listdir(self.path)
-        images = []
-        masks = []
-        for sample in data : 
-            
-            if sample.endswith(".jpg"):
-                images.append(sample)
-                
-            elif sample.endswith(".png"):
-                masks.append(sample)
-                
-        return images , masks
+        if self.transform_rgb:
+            image = self.transform_rgb(image)
+        if self.transform_grey:
+            mask = self.transform_grey(mask)
+        
+        return image , mask
 
         
