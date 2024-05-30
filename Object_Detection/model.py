e508ab46705f7bf646f97549070f17219ab2ca81
hasanmog <hmoughnieh2000@gmail.com>
2024-05-30 11:52:04 +0300

    update

--- Object_Detection/model.py
+++ Object_Detection/model.py
@@ -1,13 +1,22 @@
 import torch
 import torch.nn as nn
 
-
+class DetectorHead(nn.Module):
+    def __init__(self ,in_channels ,  anchors , num_classes = 6 ,):
+        super(DetectorHead, self).__init__()
+        
+        self.pred = nn.Conv2d(in_channels , anchors * (1+4+num_classes) , kernel_size = 3 , padding = 1)
+    def forward(self , x):
+        
+        x = self.pred(x)
+        return x
+        
 class Detector(nn.Module):
     def __init__(self , num_classes = 6):
         super(Detector, self).__init__()
         
         self.encoder_1 = nn.Sequential(   # 3 x 800 x 800 / 3 x 512 x 512 / 3 x 256 x 256
-            nn.Conv2d(3 , 16 , kernel_size = 3 , stride=2),
+            nn.Conv2d(3 , 16 , kernel_size = 3),
             nn.ReLU(),
             nn.BatchNorm2d(16), 
             )
@@ -16,16 +25,10 @@ class Detector(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(32),
             )
-        self.encoder_2 = nn.Sequential(
-            nn.Conv2d(16 , 32 , kernel_size = 3 , stride=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            )
         self.encoder_3 = nn.Sequential(
-            nn.Conv2d(32 , 64 , kernel_size = 3 , stride=2),
+            nn.Conv2d(32 , 64 , kernel_size = 3),
             nn.ReLU(),
             nn.BatchNorm2d(64),
-            nn.MaxPool2d(2)
             )
         self.encoder_4 = nn.Sequential(
             nn.Conv2d(64 , 128 , kernel_size = 3 , stride=2),
@@ -33,7 +36,7 @@ class Detector(nn.Module):
             nn.BatchNorm2d(128),
             )
         self.encoder_5 = nn.Sequential(
-            nn.Conv2d(128 , 256 , kernel_size = 3 , stride=2),
+            nn.Conv2d(128 , 256 , kernel_size = 3),
             nn.ReLU(),
             nn.BatchNorm2d(256),
             )
@@ -42,19 +45,16 @@ class Detector(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(512),
             )
-        self.encoder_7 = nn.Sequential(
-            nn.Conv2d(512 , 800 , kernel_size = 3 , stride=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(800),
-            nn.MaxPool2d(2)
-            )
-        self.adap_pooling = nn.AdaptiveAvgPool2d((14 , 14))
         
-        self.fc = nn.Sequential(
-            nn.Linear(800*14*14 , 1000),
-            nn.Linear(1000 , 512) , 
-            nn.Linear(512 , 256) , 
-            nn.Linear(256 , 128) ,
-        )
-        self.classifier = nn.Linear(128 , num_classes)
-        # self.detector = nn
+        self.detection = DetectorHead(512 , anchors = 5 , num_classes = num_classes)
+        
+    def forward(self , x):
+        encoders = [self.encoder_1 , self.encoder_2 , self.encoder_3 , self.encoder_4 , self.encoder_5 , self.encoder_6]
+        for encoder_layer in encoders:
+            x = encoder_layer(x)    
+        x = self.detection(x)
+        return x
+
+def posprocessing(outputs , score_threshold = 0.5 , box_threshold = 0.5):
+    
+    boxes , scores , classes = outputs['boxes'] , outputs['scores'] , outputs['class_probs']
